e006082c9db2c45dfb83a87a66423550d065a6bc
Kirill Golubjov <kirillcnz@gmail.com>
2023-02-19 13:00:45 +0200

    navbar, footer, sidebar, products context setup

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Home from './HomePage';
+import Products from './ProductsPage';
+import SingleProduct from './SingleProductPage';
+import Error from './ErrorPage';
+import PrivateRoute from './PrivateRoute';
+import About from './AboutPage';
+import Cart from './CartPage';
+import Checkout from './CheckoutPage';
+
+export {
+  Home,
+  Products,
+  SingleProduct,
+  About,
+  Cart,
+  Error,
+  Checkout,
+  PrivateRoute,
+};
