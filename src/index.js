64f865c3d731629b0dac00235d263279518817b1
Kirill Golubjov <kirillcnz@gmail.com>
2023-02-23 14:28:02 +0200

    Autho0-setup and etc, useContext setip

--- src/index.js
+++ src/index.js
@@ -11,12 +11,24 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+// const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+// const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
 root.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>
+  <Auth0Provider
+    domain='dev-x0mlue2qeod7lout.us.auth0.com'
+    clientId='Hcc4w3K2zpsWoNYRyIzGoTkrILxKTGIp'
+    cacheLocation='localstorage'
+    redirectUri={window.location.origin}
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>
 );
