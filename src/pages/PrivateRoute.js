c8f60cdf2fe0b9c502641fd4dd9d4d2d62a2b423
Kirill Golubjov <kirillcnz@gmail.com>
2023-02-24 09:22:03 +0200

    fix

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -1,5 +1,4 @@
 import { Route, Redirect } from 'react-router-dom';
-import { useUserContext } from '../context/user_context';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const PrivateRoute = ({ children, ...rest }) => {
