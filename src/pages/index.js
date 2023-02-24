c3d793c9d9ce745397f25d4e292bb0ba1a487770
Kirill Golubjov <kirillcnz@gmail.com>
2023-02-24 09:10:00 +0200

    AuthWrapper

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import PrivateRoute from './PrivateRoute';
 import About from './AboutPage';
 import Cart from './CartPage';
 import Checkout from './CheckoutPage';
+import AuthWrapper from './AuthWrapper';
 
 export {
   Home,
@@ -16,4 +17,5 @@ export {
   Error,
   Checkout,
   PrivateRoute,
+  AuthWrapper,
 };
